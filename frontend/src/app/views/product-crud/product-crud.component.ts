1aba7f549a8f828a770e2b5e9357746186f5ffdb
Urias Pereira <urias.pereira@soluarq.com.br>
2022-03-31 17:36:25 -0300

    Módulo angular finalizado

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,6 +1,8 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
+import { HeaderData } from './../../components/template/header/header-data-model';
 
 @Component({
   selector: 'app-product-crud',
@@ -9,7 +11,16 @@ import { Router } from '@angular/router'
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private headerService: HeaderService
+  ) {
+    headerService.HeaderData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
+  }
 
   ngOnInit(): void {
   }
